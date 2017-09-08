9d9ed5fc9533e5a2d9d92d2de00afc0dd27d7b2b
Eric Glover <etgt28@mail.missouri.edu>
2017-09-08 12:13:43 -0500

    async js server code complete

--- server/server.js
+++ server/server.js
@@ -8,44 +8,44 @@ const baseUrl = "https://www.quandl.com/api/v3/datasets/EOD";
 //api endpoints
 server.set("port", 3001);
 
+//helper function
 function addUrlQueryParams(url, queryObj) {
   Object.keys(queryObj).forEach(key => (url += `&${key}=${queryObj[key]}`));
   return url;
 }
 
+//server endpoint for getting all stock data
 server.get("/api/stocks", async (req, res) => {
-  const tickerArray = ["AAPL"];
+  const tickerArray = ["AAPL"]; //TODO: add all the tickers
   const urlArray = tickerArray.map(ticker =>
     addUrlQueryParams(`${baseUrl}/${ticker}.json?api_key=${API_KEY}`, req.query)
   );
   let data;
+  let result;
   try {
-    console.log("running here");
     data = urlArray.map(url => {
-      console.log("url: ", url);
       return fetch(url);
     });
-    await Promise.all(data);
-    console.log("data: ", data);
-    data = urlArray.map(buffer => {
+    let promises = await Promise.all(data);
+    promises = promises.map(buffer => {
       return buffer.json();
     });
-    console.log(data);
-    await Promise.all(data);
+    result = await Promise.all(promises);
   } catch (err) {
     return res.json({
       error: err,
       message: "Something went wrong with the fetching"
     });
   }
+  return res.json(result);
 });
 
+//individual stock endpoint :: not supported
 server.get("/api/stocks/:ticker", (req, res) => {
   let url = `${baseUrl}/${req.params.ticker}.json?api_key=${API_KEY}`;
   url = addUrlQueryParams(url, req.query);
-  console.log(url);
 
-  return res.send("doing something");
+  return res.send("COME BACK AGAIN LATER");
 });
 
 const port = server.listen(server.get("port"), () => {
