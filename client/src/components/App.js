8b98b30021e91ad6130725fa489d88a188790367
Eric Glover <etgt28@mail.missouri.edu>
2017-09-08 13:05:59 -0500

    client side routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,20 +7,37 @@ import "react-datetime-slider/css/ReactDatetimeSlider.css";
 import "rc-slider/assets/index.css";
 import "rc-tooltip/assets/bootstrap.css";
 
-const moment = require("moment");
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+//Portfolio, trade, transaction
+import Portfolio from "./Portfolio";
+import Trade from "./Trade";
+import Transactions from "./Transactions";
+
+// const moment = require("moment");
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <DatetimeSlider
-          min={moment().subtract(1, "hour").valueOf()}
+        {/* <DatetimeSlider
+          min={moment()
+            .subtract(1, "hour")
+            .valueOf()}
           max={moment().valueOf()}
           onChange={console.log}
         />
         <h1>Stock Portfolio App</h1>
         <Slider />
-        <Range />
+        <Range /> */}
+        <h1>Stock Portfolio App</h1>
+        <Router>
+          <Switch>
+            <Route exact path="/Portfolio" component={Portfolio} />
+            <Route exact path="/Transactions" component={Transactions} />
+            <Route exact path="/Trade" component={Trade} />
+          </Switch>
+        </Router>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
+// import "./index.css";
+import App from "./Components/App";
+// import registerServiceWorker from "./registerServiceWorker";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
